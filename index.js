7b977ba2a5093764f920591775184923f6f2130a
Dayeane <dayeane85@gmail.com>
2021-10-11 16:08:15 -0700

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result 
+  if (someNumber <= 400) { 
+    return ('This one is on me!');
+  }
+  if (someNumber >= 2000) {
+    if (someNumber >= 2500){
+      return ('No can do.');
+    }
+    return ('I will gladly take your thirty bucks.');
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return ('Ok, sounds good.');
+  } else {
+    return ('No go.');
+  }
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipType){
+  if (tipType === 'generous'){
+    return ('Thank you so much.');
+  } else if (tipType === 'not as generous') {
+    return ('Thank you.');
+  } else {
+    return ('Bye.');
+  }
 }
